b47816dea33c187dc1d484654ada7fd9b7c12245
hz2432 <hz2432@columbia.edu>
2017-04-20 17:40:36 -0400

    Add files via upload

--- Contacts/ListTableViewController.swift
+++ Contacts/ListTableViewController.swift
@@ -10,86 +10,85 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    var personList = [Person]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData { (list) in
+            self.personList += list
+            self.tableView.reloadData()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // asynchronous closure
+    private func loadData(_ completion: @escaping (_ list: [Person])->()) -> (){
+        DispatchQueue.global().async {
+            print("loading")
+            Thread.sleep(forTimeInterval: 1)
+            var arrayM = [Person]()
+            
+            for i in 0..<20{
+                let p = Person()
+                p.name = "John - \(i)"
+                p.phone = "1860" + String(format: "%06d", arc4random_uniform(100000))
+                p.title = "friend"
+                arrayM.append(p)
+            }
+            
+            DispatchQueue.main.async(execute: {
+                completion(arrayM);
+            })
+        }
+    }
+    
+    @IBAction func newPerson(_ sender: Any) {
+        performSegue(withIdentifier: "list2detail", sender: nil)
+        
+        
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    //MARK: Controller Jump method
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! DetailViewController
+        //Set choosed persom
+        if let indexPath = sender as? IndexPath{
+            vc.person = personList[indexPath.row]
+            vc.completionCallBAck = {
+                //refresh row
+                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+            //new Person
+        else{
+            vc.completionCallBAck = {
+                //get detailcontroller person
+                guard let p = vc.person else{
+                    return
+                }
+                //insert new data
+                self.personList.insert(p, at: 0)
+                
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
+    //MARK: Agent
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //run segue
+        performSegue(withIdentifier: "list2detail", sender: indexPath)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return personList.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = personList[indexPath.row].name
+        cell.detailTextLabel?.text = personList[indexPath.row].phone
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Contacts/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Contacts
+//
+//  Created by haiki on 4/20/17.
+//  Copyright © 2017 haiki. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String?
+    var phone: String?
+    var title: String?
+}
